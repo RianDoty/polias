9cfc0c8ee5966ec783d186f8458c13e955bd8290
Glitch (polias) <none>
2021-12-10 15:07:49 +0000

    👨‍❤️‍👨⛲️ Updated with Glitch

--- server/card-manager.js
+++ server/card-manager.js
@@ -3,10 +3,17 @@ const avatars = require('../src/components/game/avatars')
 function randomFromTable(tbl) {
   const values = Object.entries(tbl);
   const randomIndex = Math.floor(Math.random() * values.length);
+  
   return values[randomIndex] //[key, value]
 }
 class CardManager {
   constructor() {
+    const [, defaultPack] = randomFromTable(avatars);    
+    
+    this.assignPack(defaultPack);
+  }
+  
+  assignPack(pack) {
     
   }
 }
