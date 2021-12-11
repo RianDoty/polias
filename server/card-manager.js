b4a328511fd6e04e8c1e83d355a55756a5c0a031
Glitch (polias) <none>
2021-12-11 00:50:44 +0000

    🐮🐴 Updated with Glitch

--- server/card-manager.js
+++ server/card-manager.js
@@ -1,19 +1,68 @@
 const avatars = require('../src/components/game/avatars')
 
-function randomFromTable(tbl) {
-  const values = Object.entries(tbl);
-  const randomIndex = Math.floor(Math.random() * values.length);
-  
-  return values[randomIndex] //[key, value]
+function randomInt(max=0, min=0) {
+  return min + Math.floor(Math.random() * max)
 }
+
+function randomFromTable(tbl) {
+  const values = Object.values(tbl);
+  const randomIndex = randomInt(values.length);
+  
+  return values[randomIndex]
+}
+
+function randomFromArray(arr) {
+  return arr[randomInt(arr.length)]
+}
+
 class CardManager {
   constructor() {
-    const [, defaultPack] = randomFromTable(avatars);    
+    const defaultPack = randomFromTable(avatars);    
+    
+    this.users = {};
     
     this.assignPack(defaultPack);
   }
   
+  
+  
   assignPack(pack) {
+    this.cardPack = pack;
+    const unassignedCards = Object.keys(pack);
+    this.notEnoughCards = false;
+    
+    //Eliminate ids that users are sitting on
+    Object.values(this.users)
+      .map(u => u.cardId)
+      .forEach(id => unassignedCards.filter(c => c !== id))
+    
+    //Swap ids of users whose cards now don't exist
+    Object.values(this.users).forEach(user => {
+      if (pack.indexOf(user.cardId) === -1) {
+        this.assignCard(user); 
+      }
+    })
+  }
+  
+  assignCard(user, id) {
+    const cardIsAssigned = this.unassignedCards.indexOf(id) === -1
+    
+    let cardId;
+    if (!id || cardIsAssigned) {
+      cardId = randomFromArray(this.unassignedCards) || -1;
+    } else {
+      cardId = id
+    }
+    
+    this.users[user.id] = user;
+    user.cardId = cardId
+    
+    if (cardId === -1) { //a card couldn't be found
+      this.notEnoughCards = true;
+    }
+  }
+  
+  alertNotEnoughCards() {
     
   }
 }
