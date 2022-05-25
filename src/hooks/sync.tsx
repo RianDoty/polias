4fc13af58e524fd500bf84a7a84b68cd216aa8fa
RianDoty <95896107+RianDoty@users.noreply.github.com>
2022-05-25 15:15:09 -0700

    initial commit

--- src/hooks/sync.tsx
+++ src/hooks/sync.tsx
@@ -1,60 +1,71 @@
-import { EffectCallback, useEffect, useState } from "react";
-import useVolatileState from "./volatile-state";
+import { useEffect, useState } from "react";
 import useSocketCallbacks from "./socket-callbacks";
-import type { ListSyncKeywords, SyncClientEvents, SyncServerEvents } from "../../server/sync";
+import type {
+  ListSyncKeywords,
+  SyncClientEvents,
+  SyncServerEvents
+} from "../../server/sync";
 import { Socket } from "socket.io-client";
 
 function JSONClone<O extends object>(obj: O): O {
-  return JSON.parse(JSON.stringify(obj))
+  return JSON.parse(JSON.stringify(obj));
 }
 
 function patch<D extends object>(d: D, diff: Partial<D>) {
   for (const [key, value] of Object.entries(diff)) {
-    function hasKey(tbl: {}): tbl is {[index: typeof key]: unknown} {
-      return tbl.hasOwnProperty(key)
+    function hasKey(tbl: {}): tbl is { [index: typeof key]: unknown } {
+      return tbl.hasOwnProperty(key);
     }
 
     if (!hasKey(d) || typeof value !== typeof d[key]) {
-      Object.assign(d, {key: value})
+      Object.assign(d, { key: value });
       continue;
     }
-    
-    if (value && typeof value === 'object') {
-      const dk = d[key]
-      if (dk && typeof dk === 'object') {
-        patch(value, dk)
+
+    if (value && typeof value === "object") {
+      const dk = d[key];
+      if (dk && typeof dk === "object") {
+        patch(value, dk);
         continue;
       }
     }
-    
-    if (value === undefined) delete d[key]
 
-    Object.assign(d, {key: value})
+    if (value === undefined) delete d[key];
+
+    Object.assign(d, { key: value });
   }
-  return d
+  return d;
 }
 
-export default function useSync<k extends keyof ListSyncKeywords>(socket: Socket<SyncServerEvents, SyncClientEvents>, keyword: k, def: JSONObj = {}, log = false): [boolean, ListSyncKeywords[k], Function] {
+export default function useSync<k extends keyof ListSyncKeywords>(
+  socket: Socket<SyncServerEvents, SyncClientEvents>,
+  keyword: k,
+  def: ListSyncKeywords[k] = {},
+  log = false
+): [boolean, ListSyncKeywords[k], Function] {
   const [loading, setLoading] = useState(true);
   const [store, setStore] = useState(def);
 
   useEffect(() => {
-    socket.emit("sync_subscribe", keyword)
+    setLoading(true);
+    socket.emit("sync_subscribe", keyword);
     return () => void socket.emit("sync_unsubscribe", keyword);
-  }, [keyword]);
+  }, [keyword, socket]);
 
   function route<A extends unknown[]>(callback: (...args: A) => void) {
-    return (kw: string, ...args: A) => {if (kw === keyword) callback(...args)}
+    return (kw: k, ...args: A) => {
+      if (kw === keyword) callback(...args);
+    };
   }
 
   const onData = route((data: ListSyncKeywords[k]) => {
-    setStore(data)
-    setLoading(false)
-  })
+    setStore(data);
+    setLoading(false);
+  });
 
   const onDiff = route((diff: Partial<ListSyncKeywords[k]>) => {
-    setStore((store: object) => patch(JSONClone(store), diff))
-  })
+    setStore((store: ListSyncKeywords[k]) => patch(JSONClone(store), diff));
+  });
 
   useSocketCallbacks(socket, {
     sync_data: onData,
@@ -62,4 +73,4 @@ export default function useSync<k extends keyof ListSyncKeywords>(socket: Socket
   });
 
   return [loading, store, setStore];
-};
+}
