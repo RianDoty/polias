d3fff16a125cf862c8bcda3a3a4d58b0b180cd97
RianDoty <316071@students.cnusd.k12.ca.us>
2022-10-19 22:09:14 -0700

    yeah scrap it all woo

--- src/components/game/middle-content.tsx
+++ src/components/game/middle-content.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ErrorBoundary from "../error-boundary";
 
-import Chat from "./chat";
-
 const MiddleContent = () => {
   return (
     <div className="middle-content">
       <ErrorBoundary>
-        <Chat />
+
       </ErrorBoundary>
     </div>
   );
